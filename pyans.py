02178dc11f39a1f8176579eb23c33ae64b30da84
sairuk <sairuk@gmail.com>
2016-03-07 19:36:10 +1100

    Added ability to read extracted files from library, removed some redundant code

--- pyans.py
+++ pyans.py
@@ -7,9 +7,11 @@
 #
 # ISSUES:
 #  Colours aren't quite right
-#  Doesn't support all ansis correctly
+#  Doesn't support all ansi correctly
 #
 # CHANGELOG:
+#  v03: reads both zip and ans/asc etc from the libraries directory
+#       changed rendering method but still term width dependant
 #  v02: read randomly from artpacks, error checking, whitelists added
 #  v01: initial version, support .ans only
 #
@@ -19,9 +21,10 @@ from random import randint
 
 #### Base Settings
 baud = 57600        # emulated baud rate
-bits = 10	    # bits per char
+bits = 10           # bits per char
 cols = 80           # terminal cols
 cp = 'cp437'        # code page
+reset = '\033c'     # reset code for terminal
 ansi_delay = 3      # set the delay between ansi loading
 ansi_store = os.path.join(os.path.dirname(os.path.realpath(__file__)),'libraries')  # ANSi path
 
@@ -44,60 +47,56 @@ def main():
     packlist = []
     for root, dirs, files in os.walk(ansi_store):
         for names in files:
-            if os.path.splitext(names)[1] in arclist:
+            if os.path.splitext(names)[1] in arclist + anslist:
                 packlist.append(os.path.join(root,names))
 
     while True and packlist:
+        viewlist = []
         #### Select Random Pack
         pack = packlist[randint(0, len(packlist)-1)]
 
-        #### Read Archive
-        try: 
-            archive = zipfile.ZipFile(pack, 'r')
-	except:
-	    archive = None
-	    if debug:
-                print "Couldn't read: %s" % pack
-
-	if archive:
-            #### Build list of available ANSI's
-            viewlist = []
-            for ans in archive.namelist():
-                if os.path.splitext(ans)[1] in anslist:
-                    viewlist.append(ans)
-
-            #### Select Random ANSI
-            if len(viewlist) > 0:
-                ansi = viewlist[randint(0, len(viewlist)-1)]
-            else:
+        ### Process Archive into filelist
+        if os.path.splitext(pack)[1] in arclist:
+            #### Read Archive
+            try: 
+                archive = zipfile.ZipFile(pack, 'r')
+            except:
+                archive = None
                 if debug:
-                    print "No compatible files found in %s" % pack
-                ansi = False
+                    print "Couldn't read: %s" % pack
+            if archive:
+                for ans in archive.namelist():
+                    if os.path.splitext(ans)[1] in anslist:
+                        viewlist.append(archive.read(ans))
 
-            #### Process ANSI
-            if ansi:
-                anslines = None
 
-                try:
-                    anslines = archive.read(ansi)
-                except NotImplementedError:
-                    print "Failed reading %s" % ansi
+        #### Process ANSI into viewlist
+        else:
+            for ans in [pack]:
+                viewlist.append(open(ans,'r').read())
+            
 
-                if anslines:
-		    writeout("\033c") ## reset the screen
-                    for line in anslines.split(r"^[[s"):
-        	        for char in line:
-               	            writeout(char.decode(cp))
-                            sleep(baud_delay)
-	                print "\n"
+        #### Select Random ANSI
+        if len(viewlist) > 0:
+            ansi = viewlist[randint(0, len(viewlist)-1)]
+        else:
+            if debug:
+                print "No compatible files found in %s" % pack
+            ansi = False
 
-                if debug:
-                    sleep(ansi_delay)
-                    writeout("\033c")
-                    print "Displayed %s from %s" % (ansi, pack)
-        	sleep(ansi_delay) ## delay loading the next ansi
-       	    else:
-                sleep(0.2)
+        #### Process ANSI
+        writeout(reset) ## reset the screen
+        for idx, char in enumerate(ansi):
+            writeout(char.decode(cp))
+            sleep(baud_delay)
+            
+        if debug:
+            sleep(ansi_delay)
+            writeout(reset)
+            print "Displayed %s from %s" % (ansi, pack)
+        sleep(ansi_delay) ## delay loading the next ansi
+    else:
+        sleep(0.2)
 
 if __name__ == "__main__":
     if sys.version_info<(2,7,0):
